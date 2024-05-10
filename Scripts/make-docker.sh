983bdc02531987a19a50d5dac3ff70b1e8d6cd2e
sohamvalsangkar <soham.valsangkar@aitglobalinc.com>
2024-05-10 15:34:57 +0530

    made changes in scripts

--- Scripts/make-docker.sh
+++ Scripts/make-docker.sh
@@ -4,9 +4,9 @@ set -euo pipefail
 SCRIPTDIR="$(cd "$( dirname "$BASH_SOURCE[0]")" && pwd )"
 echo $SCRIPTDIR
 BUILD_NUMBER="${BUILD_NUMBER}"
-echo $BUILD_NUMBER
+AWS_DEFAULT_REGION="${AWS_DEFAULT_REGION}"
 REPO_PREFIX="${REPO_PREFIX}"
-echo $REPO_PREFIX
+
 log() { echo "$1" >&2; }
 
 TAG=${BUILD_NUMBER}
@@ -18,11 +18,15 @@ while IFS= read -d $'\0' -r dir; do
     echo $IFS
     svcname="$(basename "${dir}")"
     builddir="${dir}"
-    image="$svcname:$TAG"
+    image="$REPO_PREFIX:$svcname:$TAG"
     (
         cd "${builddir}"
+        docker system prune -f 
+        docker container prune -f
         log "Building and pushing: ${image}"
+        aws ecr get-login-password --region "${AWS_DEFAULT_REGION}" | docker login --username AWS --password-stdin "${REPO_PREFIX}"
         docker build -t "${image}" .
+        docker push "${image}"
     )
 done < <(find "${SCRIPTDIR}/../src" -mindepth 1 -maxdepth 1 -type d -print0)
 
